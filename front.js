fb9390e00b0d2e21cc3ea6d05699cb09ad65204f
Max1Truc <maxitruc.bonbec@gmail.com>
2019-11-19 20:55:21 +0100

    Update

--- front.js
+++ front.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 8080
+const port = process.env.PORT || 8080
 
 app.get('/', (req, res) => res.send(`
   Invite this bot: <a href="https://discordapp.com/oauth2/authorize?client_id=646405464346853377&scope=bot&permissions=76802">Here</a>
