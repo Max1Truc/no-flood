34e7026c8dc7f8b395be0d7e6494fa66a0ed61a0
Max1Truc <26603929+Max1Truc@users.noreply.github.com>
2019-11-21 07:02:49 +0100

    Remove express (#1)
    * Remove express
    
    * Remove import of front.js
    
    * Delete front.js

--- front.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 8080
-
-app.get('/', (req, res) => res.send(`
-  Invite this bot: <a href="https://discordapp.com/oauth2/authorize?client_id=646405464346853377&scope=bot&permissions=76802">Here</a>
-`))
-
-app.listen(port, () => console.log(`Web server listening on port ${port}!`))
